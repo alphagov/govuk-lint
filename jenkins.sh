1f6e053f59c9cd7f4df398067189cd852c366804
Paul Bowsher <paul.bowsher@gmail.com>
2015-07-08 15:39:00 +0100

    Try to run linter in --diff mode in testing

--- jenkins.sh
+++ jenkins.sh
@@ -6,6 +6,7 @@ git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
 bundle exec bin/govuk-lint-ruby lib bin/govuk-lint-ruby
+bundle exec bin/govuk-lint-ruby lib bin/govuk-lint-ruby --diff
 
 if [[ -n "$PUBLISH_GEM" ]]; then
   bundle exec rake publish_gem

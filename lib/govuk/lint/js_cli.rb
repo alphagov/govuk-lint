714934fb894768565da7c99f0025aadbe864b975
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-02-29 11:31:30 +0000

    make target dir first arg

--- lib/govuk/lint/js_cli.rb
+++ lib/govuk/lint/js_cli.rb
@@ -3,13 +3,22 @@ require "jshint"
 require "jshint/cli"
 require "jshint/reporters"
 
+module Jshint
+  class Configuration
+    def initialize(config = {})
+      @options = config
+    end
+  end
+end
+
 module Govuk
   module Lint
     class JsCLI
       def run(args = ARGV)
-        config_path = File.join(Govuk::Lint::CONFIG_PATH, "jshint/jshint.yml")
+        config = YAML.load_file(File.join(Govuk::Lint::CONFIG_PATH, "jshint/jshint.yml"))
+        config['include_paths'] = [args.first || '.']
 
-        linter = Jshint::Cli::run(:Default, nil, config_path)
+        linter = Jshint::Cli::run(:Default, nil, config)
         linter.errors.empty?
       end
     end
